a326cbe4ce5f1ebf9cd9eac1e00e53fa375e9ae6
Meera javad <meerajavad@gmail.com>
2023-03-11 14:43:05 +0530

    11 march

---
+++ src/main/java/ustbatchno3/junit5testcases/Vehicletest.java
@@ -0,0 +1,19 @@
+package ustbatchno3.junit5testcases;
+
+
+	import static org.junit.Assert.assertEquals;
+
+	import org.junit.Test;
+
+	public class Vehicletest {
+		
+		@Test
+		public void vehicaltest() {
+			//String state = "KL";
+			int dis = 7;
+			String expected = "Ernakulam";
+			assertEquals(expected,Vehicle.district(dis));
+		}
+	}
+
+
